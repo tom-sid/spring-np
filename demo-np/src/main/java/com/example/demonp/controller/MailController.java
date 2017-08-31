51a318fbbe8f66da2ca23d361480773b38b875d2
Tom Sid <tom_sid@yahoo.com>
2017-08-31 14:47:47 -0500

    Autowiring using constructor

--- demo-np/src/main/java/com/example/demonp/controller/MailController.java
+++ demo-np/src/main/java/com/example/demonp/controller/MailController.java
@@ -1,6 +1,5 @@
 package com.example.demonp.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +9,8 @@ import com.example.demonp.service.MailSender;
 public class MailController {
 	private MailSender mailSender;
 
-	@Autowired
-	public void setMailSender(MailSender mailSender) {
+	// optional @Autowired
+	public MailController(MailSender mailSender) {
 		this.mailSender = mailSender;
 	}
 
