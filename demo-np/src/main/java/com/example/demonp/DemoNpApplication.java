d6fb53a3f73bc6253247a893a4f6f9b581e1b4c8
Tom Sid <tom_sid@yahoo.com>
2017-08-31 11:14:31 -0500

    Project Setup

---
+++ demo-np/src/main/java/com/example/demonp/DemoNpApplication.java
@@ -0,0 +1,12 @@
+package com.example.demonp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNpApplication.class, args);
+	}
+}
