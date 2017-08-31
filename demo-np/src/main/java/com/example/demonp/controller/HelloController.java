5f24234210efd974355131eb5b3ae04ef26cd371
Tom Sid <tom_sid@yahoo.com>
2017-08-31 17:36:10 -0500

    Using @Value

--- demo-np/src/main/java/com/example/demonp/controller/HelloController.java
+++ demo-np/src/main/java/com/example/demonp/controller/HelloController.java
@@ -1,12 +1,16 @@
 package com.example.demonp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	@Value("${app.name}")
+	private String appName;
+
 	@RequestMapping("/hello")
 	public String hello() {
-		return "Hello, World!";
+		return "Hello, World " + appName;
 	}
 }
