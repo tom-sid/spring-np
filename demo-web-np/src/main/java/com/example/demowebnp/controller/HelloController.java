18421a77e5b06399696130b84c502526cc6f7040
Tom Sid <tom_sid@yahoo.com>
2017-09-01 11:23:15 -0500

    JSP setup

--- demo-web-np/src/main/java/com/example/demowebnp/controller/HelloController.java
+++ demo-web-np/src/main/java/com/example/demowebnp/controller/HelloController.java
@@ -1,12 +1,12 @@
 package com.example.demowebnp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 	@RequestMapping("/hello")
 	public String hello() {
-		return "Hello, World!";
+		return "hello";
 	}
 }
