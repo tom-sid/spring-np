ccf62e598661c1b7abe4a27050d71ff0020dd245
Tom Sid <tom_sid@yahoo.com>
2017-09-01 11:13:29 -0500

    Project Setup

---
+++ demo-web-np/src/main/java/com/example/demowebnp/DemoWebNpApplication.java
@@ -0,0 +1,12 @@
+package com.example.demowebnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebNpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebNpApplication.class, args);
+	}
+}
