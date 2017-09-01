43f7ad262cbfc6dfbe60a5dd0743ee7dfb3a8499
Tom Sid <tom_sid@yahoo.com>
2017-09-01 17:16:44 -0500

    How Java code can access values from messages.properties

--- demo-web-np/src/main/java/com/example/demowebnp/controller/HelloController.java
+++ demo-web-np/src/main/java/com/example/demowebnp/controller/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.demowebnp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@RequestMapping("/hello")
 	public String hello(Model model) {
-		model.addAttribute("name", "John");
+		
+		model.addAttribute("name", messageSource.getMessage("name", null, LocaleContextHolder.getLocale()));
 		return "hello";
 	}
 }
