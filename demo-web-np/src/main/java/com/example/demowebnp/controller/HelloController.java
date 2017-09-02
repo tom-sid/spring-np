ceae555d506814fdc5abc8affc86df3b4e110603
Tom Sid <tom_sid@yahoo.com>
2017-09-01 19:06:41 -0500

    @PathVariable and @RequestParam

--- demo-web-np/src/main/java/com/example/demowebnp/controller/HelloController.java
+++ demo-web-np/src/main/java/com/example/demowebnp/controller/HelloController.java
@@ -2,7 +2,9 @@ package com.example.demowebnp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demowebnp.configuration.MessageUtils;
 
@@ -17,5 +19,17 @@ public class HelloController {
 		model.addAttribute("address", MessageUtils.getMessage("address", "Houston", "Tulsa"));
 
 		return "hello";
+	}
+	
+	@RequestMapping("/hello/{id}")
+	public String helloId(Model model, @PathVariable("id") String id, @RequestParam("name") String name) {
+
+		model.addAttribute("id", id);
+		
+		model.addAttribute("name", name);
+
+		model.addAttribute("address", MessageUtils.getMessage("address", "Austin", "Dallas"));
+
+		return "hello-id";
 	}
 }
---
+++ demo-web-np/src/main/webapp/WEB-INF/views/hello-id.jsp
@@ -0,0 +1,5 @@
+<%@ include file="includes/header.jsp"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<spring:message code="hello"></spring:message> ${name}. Your id is ${id}.<br/>
+${address}
+<%@ include file="includes/footer.jsp"%>
