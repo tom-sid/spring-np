e6eb7154241173b2e4e57c0b91bfa9c981afab66
Tom Sid <tom_sid@yahoo.com>
2017-09-02 11:05:50 -0500

    Add a signup form

--- demo-web-np/src/main/java/com/example/demowebnp/configuration/MvcConfiguration.java
+++ demo-web-np/src/main/java/com/example/demowebnp/configuration/MvcConfiguration.java
@@ -9,5 +9,6 @@ public class MvcConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/signup").setViewName("signup");
 	}
 }
