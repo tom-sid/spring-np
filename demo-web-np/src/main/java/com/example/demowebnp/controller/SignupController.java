47bcdb32046dea75f81a1367ee17bd1d28062411
Tom Sid <tom_sid@yahoo.com>
2017-09-06 09:07:39 -0500

    Adding a POJO to grab form data

--- demo-web-np/src/main/java/com/example/demowebnp/controller/SignupController.java
+++ demo-web-np/src/main/java/com/example/demowebnp/controller/SignupController.java
@@ -5,20 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demowebnp.model.User;
 
 @Controller
 public class SignupController {
 	private static Logger log = LoggerFactory.getLogger(SignupController.class);
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String doSignup(@RequestParam String email, @RequestParam String name, @RequestParam String password) {
+	public String doSignup(User user) {
 
-		log.info("Email: " + email + " Name: " + name + " Password: " + password);
+		log.info("Email: " + user.getEmail() + " Name: " + user.getName() + " Password: " + user.getPassword());
 
 		return "redirect:/";
 	}
-	
+
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signup() {
 		return "signup";
